0d909b4f20a10520e48d363fbfc74e377f95f115
maryna <luv_this_world@hotmail.com>
2015-05-13 15:05:50 -0500

    Hard mode

--- dist/main.js
+++ dist/main.js
@@ -19,8 +19,15 @@ function* guessingGame() {
 			var low = yield game.ask('Too low. Try again.');
 			guess = low;
 		} else {
-			yield game.ask ('You won!', 'Do you want to try again?');
 			complete=true;
+			var win = yield game.choose ('You won! Do you want to play again?', 'Play', 'Quit');
+			if (win == 'Play') {
+				complete = false;
+				var number = Math.floor(Math.random()*100) + 1;
+				var guess = yield game.ask ('Please try to guess the number between 1 and 100.');
+			} else { 
+				yield game.say ('Goodbye!');
+			}
 		}
 	}
 }
