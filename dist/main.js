469324369ed74f4919a236853c82f13a3fefe86e
maryna <luv_this_world@hotmail.com>
2015-05-13 14:44:18 -0500

    Normal mode

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,26 @@
 function* guessingGame() {
-	console.log(yield game.say('Hey there!'));
-	console.log(yield game.say('Woah', 'This is cool'));
-	console.log(yield game.ask('Woah', 'This is cool'));
-	console.log(yield game.choose('Woah', 'This is cool', 'Hello world'));
-	console.log(yield game.say('That\'s all folks!', 'THE END'));
+	// console.log(yield game.say('Hey there!'));
+	// console.log(yield game.say('Woah', 'This is cool'));
+	// console.log(yield game.ask('Woah', 'This is cool'));
+	// console.log(yield game.choose('Woah', 'This is cool', 'Hello world'));
+	// console.log(yield game.say('That\'s all folks!', 'THE END'));
+	
+	yield game.say ('Hello! Let\'s play the Guesssing Game!');
+
+	var number = Math.floor(Math.random()*100) + 1;
+	var guess = yield game.ask ('Please try to guess the number between 1 and 100.');
+	var complete = false;
+	
+	while (!complete) {
+		if (guess>number) {
+			var high = yield game.ask('Too high. Try again.');
+			guess = high;
+		} else if (guess<number) {
+			var low = yield game.ask('Too low. Try again.');
+			guess = low;
+		} else {
+			yield game.ask ('You won!', 'Do you want to try again?');
+			complete=true;
+		}
+	}
 }
